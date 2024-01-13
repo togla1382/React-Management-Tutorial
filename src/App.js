389a89857d5be95296ed5ddc7c4de2123214e79e
Jung Hyun <togla1382@example.com>
2024-01-13 18:05:22 +0900

    Lee

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="gray-background">
       <img src={logo} lat="logo"/>
-      <h2>Let's develop management system! Good!!!</h2>
+      <h2>Let's develop management system!</h2>
     </div>
   );
 }
